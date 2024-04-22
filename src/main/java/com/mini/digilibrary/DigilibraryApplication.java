5752bc4242b7f18bdbd9ca648dc1be9737e32b4b
GeethaVM <geetha.mahendran@gmail.com>
2024-04-22 18:03:11 +0530

    new commit

--- src/main/java/com/mini/digilibrary/DigilibraryApplication.java
+++ src/main/java/com/mini/digilibrary/DigilibraryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DigilibraryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Printing from main");
 		SpringApplication.run(DigilibraryApplication.class, args);
 	}
 
