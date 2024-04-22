8a99a44473dbbb64593b15e6b90496efd96af8d3
GeethaVM <geetha.mahendran@gmail.com>
2024-04-22 18:01:42 +0530

    Initial commit

---
+++ src/test/java/com/mini/digilibrary/DigilibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.digilibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigilibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
